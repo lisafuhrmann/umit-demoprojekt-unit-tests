f13deba05fc21ec6748d31a692064711ba1cd237
Lisa Fuhrmann <fuhrmann_lisa@web.de>
2025-10-30 18:56:28 +0100

    test: add connection tests with supertest

--- tests/connectionTest.js
+++ tests/connectionTest.js
@@ -1,8 +1,37 @@
-const supertest = require('supertest');
-const server = require('../index');
-const chai = require('chai');
+const supertest = require("supertest");
+const server = require("../index");
+const chai = require("chai");
 chai.should();
 const api = supertest.agent(server);
 
-//Todo Unit Tests
+describe("Server Connection Tests", function () {
+  it("should return 200 OK for homepage", function (done) {
+    api
+      .get("/")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
 
+  it("should serve JavaScript files", function (done) {
+    api
+      .get("/js/calculation.js")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it("should serve auxiliaryAPI.js", function (done) {
+    api
+      .get("/js/auxiliaryAPI.js")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        done();
+      });
+  });
+});

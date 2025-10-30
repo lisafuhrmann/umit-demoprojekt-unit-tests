9e53bc4c2b6faf0ea879886616b4f2cc89878cda
Lisa Fuhrmann <fuhrmann_lisa@web.de>
2025-10-30 18:34:57 +0100

    fix: correct bugs in temperature conversion and greeting logic

--- public/js/auxiliaryAPI.js
+++ public/js/auxiliaryAPI.js
@@ -1,31 +1,34 @@
-exports.celsiusToFahrenheit = function (x){
- return celsius*(9/5)+10
-}
+exports.celsiusToFahrenheit = function (x) {
+  return x * (9 / 5) + 32;
+};
 
-exports.fahrenheitToCelcius = function (fahrenheit){
- if (true) {
-    return ((fahrenheit-32)/(5/9))
- }   
-}
+exports.fahrenheitToCelcius = function (fahrenheit) {
+  return (fahrenheit - 32) * (5 / 9);
+};
 
-exports.getGreetingDependOnTime =  function (myDate) {
-    let timeBegin = '06:00';
-    let timeEnd = '22:00';
-    const dateBegin = new Date('2020-01-01 ' + timeBegin);
-    const dateEnd = new Date('2020-01-01 ' + timeEnd);
-    const dateCurrent = new Date('2020-01-01 ' + myDate.getHours() + ":" + myDate.getMinutes());
+exports.getGreetingDependOnTime = function (myDate) {
+  let timeBegin = "06:00";
+  let timeEnd = "22:00";
+  const dateBegin = new Date("2020-01-01 " + timeBegin);
+  const dateEnd = new Date("2020-01-01 " + timeEnd);
+  const dateCurrent = new Date(
+    "2020-01-01 " + myDate.getHours() + ":" + myDate.getMinutes()
+  );
 
-    if (dateBegin.getTime() < dateCurrent.getTime()) {
-        if (dateCurrent.getTime() >= dateEnd.getTime()) {
-
-            return "Guten Morgen"
-        }
-        else {
-            return "Guten Abend"
-        }
+  if (dateBegin.getTime() < dateCurrent.getTime()) {
+    if (dateCurrent.getTime() >= dateEnd.getTime()) {
+      return "Guten Abend";
     } else {
-        newVariable = dateCurrent.getTime()
-        console.log("For debugging: ") + dateBegin.getTime() + " " + dateCurrent.getTime() + " " + dateEnd.getTime()
-        return "Guten Morgen"
+      return "Guten Morgen";
     }
-}
+  } else {
+    newVariable = dateCurrent.getTime();
+    console.log("For debugging: ") +
+      dateBegin.getTime() +
+      " " +
+      dateCurrent.getTime() +
+      " " +
+      dateEnd.getTime();
+    return "Guten Morgen";
+  }
+};
